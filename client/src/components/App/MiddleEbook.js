fc7d1d4cc380dffe940ccd063028aa2fe955ad64
Aecio Levy <aeciolevy@gmail.com>
2018-10-24 06:27:15 -0700

    Wrapped lazyLoadingimage

--- client/src/components/App/MiddleEbook.js
+++ client/src/components/App/MiddleEbook.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ebook from '../../imgs/e-book.png';
 import ebookplaceholder from '../../imgs/e-book-placeholder-8.png';
-import { SimpleImg, SimpleImgProvider } from 'react-simple-img';
 import DivFlex from '../styled/DivFlex';
+import logo2 from '../../imgs/Logo2.png';
+import logo2placeholder from '../../imgs/logo2-placeholder-8.png';
+
+const LazyLoadingImage = lazy(() => import(/* webpackChunkName: "LazyLoadingImage" */ 
+    '../Shared/LazyLoadingImage'));
 
 const MiddleEbook = () => {
     return (
-        <DivFlex style={{ marginTop: '15rem'}}>
-            <SimpleImgProvider
-                config={{ threshold: [0.6]}}
-            > 
-                <SimpleImg 
-                    duration={2}
-                    placeholder={ebookplaceholder}
-                    src={ebook}
-                />
-            </SimpleImgProvider>
-            <DivFlex>
-
+        <Suspense fallback={<div> Loadding ...</div>}>
+            <DivFlex style={{ marginTop: '15rem', border: '1px solid red'}}>
+                <LazyLoadingImage img={ebook} imgPlaceHolder={ebookplaceholder} duration={2} />
+                <DivFlex>
+                    <LazyLoadingImage img={logo2} imgPlaceHolder={logo2placeholder} 
+                        width={200} height={200}
+                    />
+                </DivFlex>
             </DivFlex>
-        </DivFlex>
+        </Suspense>
     );
 };
 
---
+++ client/src/components/Shared/LazyLoadingImage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SimpleImg, SimpleImgProvider } from 'react-simple-img';
+
+const LazyLoadingImage = React.memo(function LazyLoadingImage(props){
+    const { threshold, img, imgPlaceHolder, duration, height, width } = props;
+    return (
+        <SimpleImgProvider
+            config={{ threshold: [threshold || 0.5] }}
+        >
+            <SimpleImg
+                height={height || '100%'}
+                width={width || 500}
+                animationDuration={duration || 1}
+                placeholder={imgPlaceHolder}
+                src={img}
+            />
+        </SimpleImgProvider>
+    );
+});
+
+LazyLoadingImage.propTypes = {
+    img: PropTypes.string.isRequired,
+    imgPlaceHolder: PropTypes.string.isRequired,
+};
+
+export default LazyLoadingImage;
